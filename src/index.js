df4c859bdc153f81d2ba89add762d64cde8450c6
yatharth <yatharthverma070@gmail.com>
2020-04-29 14:01:13 +0530

    :hammer: Fix socket server

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const socketio = require('socket.io');
+const server = http.createServer(app);
 
+
+const port = process.env.PORT || 3000;
+const io = socketio(server);
 const publicDirectoryPath = path.join(__dirname,'../public');
+
 app.use(express.static(publicDirectoryPath))
+
+io.on('connection', () => {
+    console.log("New Web socket connection!!");
+    
+})
+
 
 app.get('/',(req,res)=>{
     res.render("This is home page!!");
 })
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log("Listening on port 3000!!!");
 })
 
