038e04fb63bf99551a7c3744970a727f4c856adb
yatharth <yatharthverma070@gmail.com>
2020-04-29 14:06:38 +0530

    :hammer: Add travis and linter

---
+++ README.md
@@ -0,0 +1,12 @@
+### Chat Application
+
+#### Tech Stack
+
+* [x] Nodejs
+* [x] Express
+* [x] Socket.io (WebSockets)
+* [x] Nodemon
+* [x] jshint (Error linter)
+
+#### What is Web Sockets ?
+
--- src/index.js
+++ src/index.js
@@ -10,19 +10,19 @@ const port = process.env.PORT || 3000;
 const io = socketio(server);
 const publicDirectoryPath = path.join(__dirname,'../public');
 
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 io.on('connection', () => {
     console.log("New Web socket connection!!");
     
-})
+});
 
 
 app.get('/',(req,res)=>{
     res.render("This is home page!!");
-})
+});
 
 server.listen(port,()=>{
     console.log("Listening on port 3000!!!");
-})
+});
 
