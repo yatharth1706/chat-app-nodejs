024f6c71597c269213d8830a4c71f46244426cc5
yatharth <yatharthverma070@gmail.com>
2020-04-30 13:09:32 +0530

    Add broadcast

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,9 @@
 const socket = io();
 
+socket.on('message', (msg) => {
+    console.log(msg);
+})
+
 // add event listener to button to send message to server
 
 var form = document.getElementById('myForm');
@@ -7,14 +11,20 @@ var form = document.getElementById('myForm');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     var msg = document.getElementById('message').value;
-    socket.emit('message', msg);
-})
-
-socket.on('message', (msg) => {
-    console.log(msg);
+    socket.emit('sendMessage', msg);
 })
 
 
+// add event listener to send message btn
+document.getElementById("send-location").addEventListener('click',()=>{
+    
+    if(!navigator.geolocation){
+        return alert("Geolocation is not supported in your browser! Sorry!!")
+    }
 
-
+    // get the location of user
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position);
+    })
+})
 
